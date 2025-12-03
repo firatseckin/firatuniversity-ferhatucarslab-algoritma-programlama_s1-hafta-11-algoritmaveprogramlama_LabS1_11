a2db248d55d3b01fbe37463c6726f8f8c19f5bfa
firatseckin <250541042@firat.edu.tr>
2025-12-03 21:39:53 +0300

    Refactor Exercise4_1 to DatePrinter with date formats

--- Exercise4_1.java
+++ Exercise4_1.java
@@ -1,14 +1,25 @@
-public class Exercise4_1 {
-
-    public static void printAmerican(String day, int date, String month, int year) {
-        // TODO
-    }
-
-    public static void printEuropean(String day, int date, String month, int year) {
-        // TODO
-    }
+public class DatePrinter {
 
     public static void main(String[] args) {
-        // TODO: test methods
+        String day = "Monday";
+        String month = "July";
+        int date = 22;
+        int year = 2019;
+
+        System.out.println("Amerikan Formatı:");
+        printAmerican(day, date, month, year);
+
+        System.out.println("Avrupa Formatı:");
+        printEuropean(day, date, month, year);
+    }
+
+    // Amerikan Formatı: Monday, July 22, 2019
+    public static void printAmerican(String day, int date, String month, int year) {
+        System.out.println(day + ", " + month + " " + date + ", " + year);
+    }
+
+    // Avrupa Formatı: 22 July 2019, Monday
+    public static void printEuropean(String day, int date, String month, int year) {
+        System.out.println(date + " " + month + " " + year + ", " + day);
     }
 }
